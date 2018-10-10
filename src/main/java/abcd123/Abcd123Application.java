cff236f36cc4a19f087439502bd24bd0daeadf7f
Matt Cadorette <mcadorette@gmail.com>
2018-10-10 08:29:58 -0400

    Initial commit from Atomist

---
+++ src/main/java/abcd123/Abcd123Application.java
@@ -0,0 +1,12 @@
+package abcd123;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Abcd123Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Abcd123Application.class, args);
+	}
+}
